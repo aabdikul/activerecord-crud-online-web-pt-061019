ad2bd53bb06d201d74fd14264fc2413e3155887a
Aida Abdikulova <abdikulova@gmail.com>
2019-09-28 03:16:31 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -6,7 +6,7 @@
 # end                              # end
 
 def can_be_instantiated_and_then_saved
-  movie = Movie.create
+  movie = Movie.new
   movie.title = "This is a title."
   movie.save
 end
@@ -20,35 +20,37 @@ def can_be_created_with_a_hash_of_attributes
       lead: "Paul Newman",
       in_theaters: false
   }
-  movie = __
+  movie = Movie.new(attributes)
+  movie.save
+  movie
 end
 
-def can_be_created_in_a_block(args = __)
+def can_be_created_in_a_block(args = {title:"Home Alone",release_date:1990})
   # If no arguments are passed, use default values:
   # title == "Home Alone"
   # release_date == 1990
 
   Movie.create do |m|
-    __
+    m.title = args[:title]
+    m.release_date = args[:release_date]
+    m.save
   end
 end
 
 def can_get_the_first_item_in_the_database
-  #sql <<-SQL
-  #SELECT * FROM movies
-  #SQL
+  Movie.first
 end
 
 def can_get_the_last_item_in_the_database
-  __
+  Movie.last
 end
 
 def can_get_size_of_the_database
-  __
+  Movie.count
 end
 
 def can_find_the_first_item_from_the_database_using_id
-  __
+  Movie.find_by(:id=>1)
 end
 
 def can_find_by_multiple_attributes
@@ -56,13 +58,13 @@ def can_find_by_multiple_attributes
   # title == "Title"
   # release_date == 2000
   # director == "Me"
-  __
+  Movie.find_by(:title=>"Title",:release_date=>2000,:director=>"Me")
 end
 
 def can_find_using_where_clause_and_be_sorted
   # For this test return all movies released after 2002 and ordered by
   # release date descending
-  __
+  Movie.find_by(:release_date=>2002)
 end
 
 def can_be_found_updated_and_saved
